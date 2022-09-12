41e523c36e4299d2411ff1d15ae3949b0fc5e05b
Florian Braun <florian.braun.2005@gmail.com>
2022-09-12 20:38:15 +0200

    adding the first mini-project

---
+++ Mailing_Address.py
@@ -0,0 +1,9 @@
+First_Name = "Florian"
+Last_Name = "Braun"
+Address_Number = "14"
+Address_Street = "Rue Paul Wigreux"
+Address_Region = "Howald"
+Address_Country = "Luxemburg"
+Address_Postal = "LU-2727"
+
+print(First_Name + ' ' + Last_Name + '\n' + Address_Number + ', ' + Address_Street + '\n' + Address_Region + ', ' + Address_Country + '\n' + Address_Postal)
