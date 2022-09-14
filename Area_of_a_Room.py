fd62bb47e3235bc42076516177a66b72a6f14f09
Florian Braun <florian.braun.2005@gmail.com>
2022-09-14 12:13:50 +0200

    adding the question to the exercise

--- Area_of_a_Room.py
+++ Area_of_a_Room.py
@@ -1,3 +1,6 @@
+#Question: Write a program that asks the user to enter the width and length of a room. Once these values have been read, your program should compute and display the area of the room. The length and the width will be entered as floating-point numbers. Include units in prompt and output message; either feet or meters, depending on which unit you are more confortable working with.
+#Criteria: Exercise 3: Area of a Room (Solved, 13 Lines)
+
 Unit = input("what unit would you want to use (Metric or Imperial): \n")
 Width = input("Width of the room: \n")
 Length = input("Length of the room: \n")
