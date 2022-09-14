a487ccd1d08549569d3a14ee88f3bce31a468e22
Florian Braun <florian.braun.2005@gmail.com>
2022-09-14 12:13:55 +0200

    adding the question to the exercise

--- Area_of_a_Field.py
+++ Area_of_a_Field.py
@@ -1,3 +1,6 @@
+#Question: Create a program that reads the length and width of a farmer's field from the user in foot. Display the area of the field in acres.
+#Criteria: Exercise 4: Area of a Field (solved, 15 Lines)
+
 Width = input("Width of the field (feet): \n")
 Length = input("Length of the field (feet): \n")
 
