f7fc86050ca33cacb861d871ef6b5cf8c119a9c3
Florian Braun <florian.braun.2005@gmail.com>
2022-09-14 12:13:43 +0200

    adding the question to the exercise

--- Hello.py
+++ Hello.py
@@ -1,3 +1,6 @@
+#Question: Write a program that asks the user to enter his or her name. The program should respond with a message that says hello to the user, using his or her name.
+#Criteria: Exercise 2: Hello (9 Lines)
+
 First_Name = input("What is your First Name: \n")
 Last_Name = input("What is your Second Name: \n")
 Gender = input("What is your gender: \n")
