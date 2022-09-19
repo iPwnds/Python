5e0ebd280db992195495619eb1a7a1a7f8936127
Florian Braun <florian.braun.2005@gmail.com>
2022-09-19 13:43:20 +0200

    adding the question to the exercise

--- Bottle_Deposits.py
+++ Bottle_Deposits.py
@@ -1,3 +1,6 @@
+#Question: In many jurisdictions a small deposit is added to drink containers to encourage people to recycle them. In one particular jurisdiction, drink containers holding one liter or less have a SO.10 deposit, and drink containers holding more than one liter have a $0.25 deposit. Write a program that reads the number of containers of each size from the user. Your program should continue by computing and displaying the refund that will be received for returning those containers. Format the output so that it includes a dollar sign and two digits to the right of the decimal point.
+#Criteria: Exercise 6: Bottle Deposits (solved, 15 Lines)
+
 Amount = int(input("How many drink containers do you have: \n"))
 Capacity = float(input("What are the size's of the drink containers (in litres): \n"))
 Total = 0
