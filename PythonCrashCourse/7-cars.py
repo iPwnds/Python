18a4f4af11da37e085d6a8355ae6808b27730519
Florian Braun <florian.braun.2005@gmail.com>
2022-12-17 13:14:34 +0100

    moving to the new folder

---
+++ PythonCrashCourse/7-cars.py
@@ -0,0 +1,6 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort() #the sort method sort a list by alphabetical order (once you use this method to sort a list, you can not go back to the original list)
+print(cars)
+
+cars.sort(reverse=True) #this will sort the list in the reverse alphabetical order
+print(cars)
