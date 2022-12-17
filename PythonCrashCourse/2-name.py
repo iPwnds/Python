f6c3e98bddc83c6f25c9c361af3808fe9da3f5de
Florian Braun <florian.braun.2005@gmail.com>
2022-12-17 13:14:22 +0100

    moving to the new folder

---
+++ PythonCrashCourse/2-name.py
@@ -0,0 +1,4 @@
+name = "ada lovelace"
+print(name.title()) #this will print the name var with uppercase letters at the start of the words
+print(name.upper()) #this will print the name var in all uppercase letters
+print(name.lower()) #this will print the name var in all lowercase letters
