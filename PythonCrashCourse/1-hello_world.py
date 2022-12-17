ce272c9c636d389de0816d1b61fed8f3be608710
Florian Braun <florian.braun.2005@gmail.com>
2022-12-17 13:14:09 +0100

    moving to the new folder

---
+++ PythonCrashCourse/1-hello_world.py
@@ -0,0 +1,4 @@
+print("Hello Python world!") #this will print what is in the quotation marks
+
+message ="Hello Python world!" #this sets a value for the message variable
+print(message) #prints the message variable (var)
