6b52808b814aa4dc03da8b02cd43922a1b3047a1
Florian Braun <florian.braun.2005@gmail.com>
2022-12-17 13:14:27 +0100

    moving to the new folder

---
+++ PythonCrashCourse/4-apostrophe.py
@@ -0,0 +1,19 @@
+message = "One of Python's strengts is its diverse community."
+print(message)
+
+#message = 'One of Python's strengts is its diverse community.' #this wont work because there is a single quote inside of the single quotes
+#print(message)
+
+print(2 + 2) #you can add 2 integers
+print(6 - 3) #you can substract 2 integers
+print(4.5 * 8.0) #you can multiply 2 floats (answer is given in a float becouse there are floats in the equasion)
+print(10 / 5) #you can divide 2 integers (answer is given in a float becouse the answer might be a float even tho there are no floats used in the equasion)
+print(6 ** 2) #you can put one integer to the power of another
+print(123_456_789) #you can group digits by using underscores (to make it more readable)
+
+x, y, z = 4, 8, 2 #you can assigne multiple values to multiple variables like this
+print(z, x) #print the z and x values
+
+THIS_IS_A_CONSTANT = 10 #the THIS_IS_A_CONSTANT variable is a variable that can never change (is constant)
+
+# this is a comment python ignores it and it is used to explain what a certain piece of code is supposed to do/make a summary
