db5650202c4a5449510cbbfd33ed3a4deeb0151b
Florian Braun <florian.braun.2005@gmail.com>
2022-12-20 21:09:26 +0100

    funny movie hack like screen

--- matrix.py
+++ matrix.py
@@ -1,5 +1,9 @@
 import secrets
 import string
+import pyfiglet
+import colorama
+
+colorama.init()
 
 letters = string.ascii_letters
 digits = string.digits
@@ -7,13 +11,19 @@ special_chars = string.punctuation
 
 alphabet = letters + digits + special_chars
 
-digits = 267
+digits = 236
 
-screen = 1
+screen = 10000
 
-while screen == 1:
+while screen > 0:
     sequence = ''
     for i in range(digits):
         sequence += ''.join(secrets.choice(alphabet))
 
     print(sequence)
+    screen = screen -1
+
+access = pyfiglet.figlet_format("Access Granted")
+print(colorama.Fore.GREEN + access)
+
+colorama.deinit()
