5af20c73862016a168373035120c80fe756ba305
Florian Braun <florian.braun.2005@gmail.com>
2022-12-20 21:08:39 +0100

    A file github copilot made (pretty cool to see)

---
+++ AI.py
@@ -0,0 +1,142 @@
+#write a story
+def write_story():
+    #get the user's name
+    name = input("Enter your name: ")
+    #get the user's age
+    age = int(input("Enter your age: "))
+    #get the user's hometown
+    hometown = input("Enter your hometown: ")
+    #get the user's favorite food
+    food = input("Enter your favorite food: ")
+    #get the user's favorite color
+    color = input("Enter your favorite color: ")
+    #get the user's favorite animal
+    animal = input("Enter your favorite animal: ")
+    #get the user's favorite sport
+    sport = input("Enter your favorite sport: ")
+    #get the user's favorite movie
+    movie = input("Enter your favorite movie: ")
+    #get the user's favorite song
+    song = input("Enter your favorite song: ")
+    #get the user's favorite book
+    book = input("Enter your favorite book: ")
+    #get the user's favorite game
+    game = input("Enter your favorite game: ")
+    #get the user's favorite TV show
+    tv_show = input("Enter your favorite TV show: ")
+    #get the user's favorite holiday
+    holiday = input("Enter your favorite holiday: ")
+    #get the user's favorite season
+    season = input("Enter your favorite season: ")
+    #get the user's favorite weather
+    weather = input("Enter your favorite weather: ")
+    #get the user's favorite drink
+    drink = input("Enter your favorite drink: ")
+    #get the user's favorite dessert
+    dessert = input("Enter your favorite dessert: ")
+    #get the user's favorite fruit
+    fruit = input("Enter your favorite fruit: ")
+    #get the user's favorite vegetable
+    vegetable = input("Enter your favorite vegetable: ")
+    #get the user's favorite ice cream flavor
+    ice_cream = input("Enter your favorite ice cream flavor: ")
+    #get the user's favorite candy
+    candy = input("Enter your favorite candy: ")
+    #get the user's favorite restaurant
+    restaurant = input("Enter your favorite restaurant: ")
+    #get the user's favorite store
+    store = input("Enter your favorite store: ")
+    #get the user's favorite brand
+    brand = input("Enter your favorite brand: ")
+    #get the user's favorite website
+    website = input("Enter your favorite website: ")
+    #get the user's favorite social media platform
+    social_media = input("Enter your favorite social media platform: ")
+    #get the user's favorite app
+    app = input("Enter your favorite app: ")
+    #get the user's favorite video game
+    video_game = input("Enter your favorite video game: ")
+    #get the user's favorite sport team
+    sport_team = input("Enter your favorite sport team: ")
+    #get the user's favorite sport player
+    sport_player = input("Enter your favorite sport player: ")
+    #get the user's favorite movie star
+    movie_star = input("Enter your favorite movie star: ")
+    #get the user's favorite singer
+    singer = input("Enter your favorite singer: ")
+    #get the user's favorite actor
+    actor = input("Enter your favorite actor: ")
+    #get the user's favorite actress
+    actress = input("Enter your favorite actress: ")
+    #get the user's favorite author
+    author = input("Enter your favorite author: ")
+    #get the user's favorite YouTuber
+    youtuber = input("Enter your favorite YouTuber: ")
+    #get the user's favorite Instagrammer
+    instagrammer = input("Enter your favorite Instagrammer: ")
+    #get the user's favorite Twitter user
+    twitter_user = input("Enter your favorite Twitter user: ")
+    #get the user's favorite Snapchat user
+    snapchat_user = input("Enter your favorite Snapchat user: ")
+    #get the user's favorite TikTok user
+    tiktok_user = input("Enter your favorite TikTok user: ")
+    #get the user's favorite Facebook user
+    facebook_user = input("Enter your favorite Facebook user: ")
+    #get the user's favorite Reddit user
+    reddit_user = input("Enter your favorite Reddit user: ")
+    #get the user's favorite Twitch user
+    twitch_user = input("Enter your favorite Twitch user: ")
+    #get the user's favorite LinkedIn user
+    linkedin_user = input("Enter your favorite LinkedIn user: ")
+    #get the user's favorite Pinterest user
+    pinterest_user = input("Enter your favorite Pinterest user: ")
+    #get the user's favorite Tumblr user
+    tumblr_user = input("Enter your favorite Tumblr user: ")
+    #get the user's favorite Wikipedia user
+    wikipedia_user = input("Enter your favorite Wikipedia user: ")
+    #get the user's favorite Quora user
+    quora_user = input("Enter your favorite Quora user: ")
+    #get the user's favorite Stack Overflow user
+    stackoverflow_user = input("Enter your favorite Stack Overflow user: ")
+    #get the user's favorite GitHub user
+    github_user = input("Enter your favorite GitHub user: ")
+    #get the user's favorite BitBucket user
+    bitbucket_user = input("Enter your favorite BitBucket user: ")
+    #get the user's favorite GitLab user
+    gitlab_user = input("Enter your favorite GitLab user: ")
+    #get the user's favorite CodePen user
+    codepen_user = input("Enter your favorite CodePen user: ")
+    #get the user's favorite CodeSandbox user
+    codesandbox_user = input("Enter your favorite CodeSandbox user: ")
+    #get the user's favorite Repl.it user
+    repl_it_user = input("Enter your favorite Repl.it user: ")
+    #get the user's favorite HackerRank user
+    hackerrank_user = input("Enter your favorite HackerRank user: ")
+    #get the user's favorite HackerEarth user
+    hackerearth_user = input("Enter your favorite HackerEarth user: ")
+    #get the user's favorite LeetCode user
+    leetcode_user = input("Enter your favorite LeetCode user: ")
+    #get the user's favorite CodeChef user
+    codechef_user = input("Enter your favorite CodeChef user: ")
+    #get the user's favorite CodeForces user
+    codeforces_user = input("Enter your favorite CodeForces user: ")
+    #get the user's favorite TopCoder user
+    topcoder_user = input("Enter your favorite TopCoder user: ")
+    #get the user's favorite AtCoder user
+    atcoder_user = input("Enter your favorite AtCoder user: ")
+    #get the user's favorite GeeksForGeeks user
+    geeksforgeeks_user = input("Enter your favorite GeeksForGeeks user: ")
+    #get the user's favorite CodeWars user
+    codewars_user = input("Enter your favorite CodeWars user: ")
+    #get the user's favorite CodeSignal user
+    codesignal_user = input("Enter your favorite CodeSignal user: ")
+    #get the user's favorite CodeCombat user
+    codecombat_user = input("Enter your favorite CodeCombat user: ")
+    #get the user's favorite CodeEval user
+    codeeval_user = input("Enter your favorite CodeEval user: ")
+    #get the user's favorite CodeAbbey user
+    codeabbey_user = input("Enter your favorite CodeAbbey user: ")
+    #get the user's favorite CodeFights user
+    codefights_user = input("Enter your favorite CodeFights user: ")
+    #get the user's favorite CodeChef user
+    codechef_user = input("Enter your favorite CodeChef user: ")
