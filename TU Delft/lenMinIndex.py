86352c343f05ef1fefdd3fdd0efdaad5c47faa7c
iPwnds <florian.braun.2005@gmail.com>
2023-01-26 12:04:07 +0100

    second Selection Sort course

---
+++ TU Delft/lenMinIndex.py
@@ -0,0 +1,12 @@
+numbers = [42, 23, 15, 1, 8, 16, 4]
+
+print(numbers)
+
+for i in range(0,len(numbers)):
+    minimum = min(numbers)
+    min_index = numbers.index(minimum)
+    
+    numbers[min_index] = numbers[i]
+    numbers[i] = minimum
+    
+    print(numbers)
